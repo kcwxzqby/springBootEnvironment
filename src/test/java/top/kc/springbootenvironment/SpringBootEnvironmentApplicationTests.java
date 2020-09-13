b115c5797fa7f0e08f4457dd8f6d68207b5dc593
kc <kcwxzqby@gmail.com>
2020-09-13 19:40:43 +0800

    init

---
+++ src/test/java/top/kc/springbootenvironment/SpringBootEnvironmentApplicationTests.java
@@ -0,0 +1,13 @@
+package top.kc.springbootenvironment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEnvironmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
